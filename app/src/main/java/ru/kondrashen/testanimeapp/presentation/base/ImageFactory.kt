3c99c91388f3c30bc5ce0388b1ba97a49d710636
DanKonad <dankondrashen@yandex.com>
2024-09-03 13:18:40 +0300

    обновление UI (центрирование списков и стили для текста)

--- app/src/main/java/ru/kondrashen/testanimeapp/presentation/base/ImageFactory.kt
+++ app/src/main/java/ru/kondrashen/testanimeapp/presentation/base/ImageFactory.kt
@@ -1,11 +1,9 @@
 package ru.kondrashen.testanimeapp.presentation.base
 
-import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import ru.kondrashen.testanimeapp.R
 
 object ImageFactory {
